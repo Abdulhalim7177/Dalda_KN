48fdb180f94039ff51332876a39ccd7dfb419fa5
Abdulhalim7177 <abdulmuhd7177@gmail.com>
2025-02-24 16:32:22 +0100

    Add admin, resident, and security dashboard views

---
+++ resources/views/resident/resident.blade.php
@@ -0,0 +1,19 @@
+<!-- resources/views/resident/dashboard.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Resident Dashboard</div>
+
+                <div class="card-body">
+                    Welcome to the Resident Dashboard!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/security/dashboard.blade.php
@@ -0,0 +1,19 @@
+<!-- resources/views/security/dashboard.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Security Dashboard</div>
+
+                <div class="card-body">
+                    Welcome to the Security Dashboard!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
